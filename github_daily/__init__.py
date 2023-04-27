ecc863432d61eb91bfea1ef115c74be2f0cf02e3
ywang <74822853+happyywang@users.noreply.github.com>
2023-04-27 10:04:07 +0200

    Update __init__.py

--- github_daily/__init__.py
+++ github_daily/__init__.py
@@ -1,6 +1,14 @@
 from .from_issues import get_info_from_issue_comments
 from .runner import GTDRunner
 
+MY_STATUS_DICT_FROM_API = {
+   # "番茄": {
+   #     "daily_func": get_forst_daily,
+   #     "url": "https://github.com/yihong0618/2023/issues/12",
+   #     "unit_str": " (个)",
+   # },
+}
+
 MY_STATUS_DICT_FROM_COMMENTS = {
     "周记": {"daily_func": get_info_from_issue_comments, "unit_str": " (周)"},
     "GTD": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
